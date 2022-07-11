0d561cbd1fe4925c04d483d599492f7ba0e0a081
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-07-11 16:59:43 +0200

    stampa cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,12 +5,21 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Document</title>
+  <link rel="stylesheet" href="{{ URL::asset('css/home.css') }}" />
 </head>
 <body>
   <h1>Lista films:</h1>
   <ul>
     @foreach ($movies as $movie)
-        <li>{{$movie->title}}</li>
+    <div class="cards-container">
+      <div class="card-box">
+        <h2>Titolo: {{$movie->title}}</h2>
+        <h4>Titolo originale: {{$movie->original_title}}</h4>
+        <h5>Nazionalità: {{$movie->nationality}}</h5>
+        <h5>Data uscita: {{$movie->date}}</h5>
+        <h5>Voto: {{$movie->vote}}</h5>
+      </div>
+    </div>
     @endforeach
   </ul>
 </body>
